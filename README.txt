9507774b9ed94e4d485927cf1cf0fc46b87cd0b4
Andres Ocampo <andresocampo1228@gmail.com>
2025-02-22 11:55:05 -0500

    Colombian data selected, and linear plot and correlation matrix as a comment, both plots need to be readjusted.

---
+++ README.txt
@@ -0,0 +1,8 @@
+This is a proyect which uses the WHO Mortality DataBase to predict the number of deaths
+in Colombia based on the sex and age group.
+
+1. We downloaded and loaded the database in the main.py file
+2. We filtered data related to Colombia
+3. We have readed, formatted and cleaned to erease null values,
+obtaining values in correct format to do mathmatical operations and
+Seeing useful columns to train a model in future.
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 file_path = 'WHOMortalityDatabase.csv'
 
 df = pd.read_csv(file_path, skiprows=6, index_col=False)
-# First 5 rows
-print(df.head())
-# Info about the data
-print(df.info())
-# Check for missing values
-print(df.isnull().sum())
+# # First 5 rows
+# print(df.head())
+# # Info about the data
+# print(df.info())
+# # Check for missing values
+# print(df.isnull().sum())
+
+# Data filtering by Colombia
+df = df[df['Country Name'] == 'Colombia']
 
 # Data Cleaning
 # Erease unnecessary columns
-df = df.drop(columns=['Region Code', 'Country Code'])
+df = df.drop(columns=['Region Code', 'Country Code', 'Age group code',
+             'Percentage of cause-specific deaths out of total deaths',
+                      'Age-standardized death rate per 100 000 standard population'])
 
 # Column formatting
 df['Number'] = pd.to_numeric(df['Number'], errors='coerce')
@@ -25,4 +32,21 @@ df['Number'] = df['Number'].fillna(means['Number'])
 df['Death rate per 100 000 population'] = df['Death rate per 100 000 population'].fillna(
     means['Death rate per 100 000 population'])
 
-print(df.isnull().sum())
+
+# Exploratory Analysis
+# Statistic summary
+print(df[['Number', 'Death rate per 100 000 population']].describe())
+print(df.info())
+# # Death rate per 100 000 population by year
+# plt.figure(figsize=(12, 6))
+# sns.lineplot(data=df, x='Year',
+#              y='Death rate per 100 000 population', hue='Country Name')
+# plt.title('Death rate per 100 000 population by year')
+# plt.show()
+
+# # Data Frame correlation matrix
+# plt.figure(figsize=(8, 6))
+# sns.heatmap(df[['Year', 'Number', 'Death rate per 100 000 population']
+#                ].corr(), annot=True, cmap='coolwarm')
+# plt.title('Correlation matrix')
+# plt.show()
